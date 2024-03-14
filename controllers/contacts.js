60ee8e4b9253b37eee16dc5388726f8ba4da7198
John Doe <johndoe@example.com>
2024-03-14 16:20:54 -0400

    Err handling added

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,21 +1,48 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
-const getAll = async (req, res) => {
-  const result = await mongodb.getDb().db().collection('contacts').find();
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists);
-  });
+
+// this code could crash 
+// const getAll = async (req, res) => {
+// const result = await mongodb.getDb().db().collection('contacts').find();
+// result.toArray().then((lists) => {
+
+// rewrite above code to let mongo store and advise of error
+const getAll = (req, res) => {
+  mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .find()
+    .toArray((err, lists) => {
+      if (err) {
+        res.status(400).json({ message: err });
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists);
+    });
 };
 
-const getSingle = async (req, res) => {
+// this code could crash
+// const getSingle = async (req, res) => {
+// const userId = new ObjectId(req.params.id);
+// const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
+// result.toArray().then((lists) => {
+
+// rewrite above code to let mongo store and advise of error
+const getSingle = (req, res) => {
   const userId = new ObjectId(req.params.id);
-  const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
-  result.toArray().then((lists) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.status(200).json(lists[0]);
-  });
+  mongodb
+    .getDb()
+    .db()
+    .collection('contacts')
+    .find({ _id: userId })
+    .toArray((err, result) => {
+      if (err) {
+      }
+      res.setHeader('Content-Type', 'application/json');
+      res.status(200).json(lists[0]);
+    });
 };
 
 const createContact = async (req, res) => {
