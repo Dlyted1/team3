6dff220ff3fc9db6e6b9c77e44a33034c939e024
John Doe <johndoe@example.com>
2024-03-14 22:14:27 -0400

    correct validate.js

--- helpers/validate.js
+++ helpers/validate.js
@@ -1,8 +1,4 @@
 const Validator = require('validatorjs');
-const validator = (body, rules, customMessages, callback)
-const validation
-
-const Validator = require('validatorjs');
 const validator = async (body, rules, customMessages, callback) => {
     const validation = new Validator(body, rules, customMessages);
     validation.passes(() => callback(null, true));
